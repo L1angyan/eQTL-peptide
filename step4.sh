dfac447413f2ea0ef3d7d4ced6796cd3533db4c9
Lyon <46277338+yannnnLi@users.noreply.github.com>
2021-07-02 14:14:21 +0800

    Update step4.sh

--- step4.sh
+++ step4.sh
@@ -4,5 +4,5 @@ module load R/3.6.0
 Rscript association.R
 
 mv peptide*GLM* ./zzz_glm
-mv peptide*MLM* ./zzz_glm
-mv peptide*FarmCPU* ./zzz_glm
+mv peptide*MLM* ./zzz_mlm
+mv peptide*FarmCPU* ./zzz_farmcpu
